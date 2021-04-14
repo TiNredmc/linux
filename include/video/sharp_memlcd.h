83dca43c900dfda9db8f1209d7159f634bb49365
TiNredmc <thipok17@gmail.com>
2021-04-14 16:09:06 +0700

    [W.I.P] SHARP memory display driver experiment
    Signed-off-by: TiNredmc <thipok17@gmail.com>

---
+++ include/video/sharp_memlcd.h
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Sharp Memory Display Driver.
+ *
+ * In this case I'm testing with LS027B7DH01
+ * But it should work with others by changing 
+ * W x H Display resolution
+ *
+ * Copyright (c) 2021 Thipok Jiamjarapan
+ * <thipok17@gmail.com> 
+ *
+ */
+
+struct memlcd_par {
+	struct spi_device *spi;
+	struct fb_info *info;
+	struct pwm_device *pwm;
+	const struct memlcd_platform_data *pdata;
+	u8 *ssbuf;
+	int virt_cs;
+	u32 pwm_period;
+};
+
+struct memlcd_platform_data {
+	int vcs_gpio;
+};
